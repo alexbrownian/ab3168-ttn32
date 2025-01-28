fc2e43647ab056000f586f8f2695a9ee8c625918
alexbrownian <56255838+alexbrownian@users.noreply.github.com>
2025-01-28 13:44:18 +0000

    Task 1C (TBC)

---
+++ Task1C.py
@@ -0,0 +1,15 @@
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import stations_within_radius
+
+def run():
+    stations = build_station_list()
+
+    cambridge_center = (52.2053, 0.1218)
+    nearby_stations = stations_within_radius(stations, cambridge_center, 10) 
+    # pull from the Geo.py doc
+
+    print(sorted([station.name for station in nearby_stations]))
+
+if __name__ == "__main__":
+    run()
+x
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,15 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+
+def stations_within_radius(stations, centre, r):
+    result = []
+    for station in stations:
+        dx = station.coord[0] - centre[0]
+        dy = station.coord[1] - centre[1]
+        distance = (dx**2 + dy**2)**0.5*111
+
+        if distance <= r:
+            result.append(station)
+    return result
+
